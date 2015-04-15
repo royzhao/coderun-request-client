04850d2982c58adaf498e23967825121e787d9c4
lovening <1433391@tongji.edu.cn>
2015-04-15 16:25:06 +0800

    test

--- sso.go
+++ sso.go
@@ -21,19 +21,21 @@ func NewSSOClient(endpoint string) (*SSOClient, error) {
 }
 
 type Login struct {
-	Is_login bool `json:"is_login" yaml:"is_login"`
-	Uid      int  `json:"u_id,omitempty" yaml:"u_id,omitempty"`
+	Is_login string `json:"is_login" yaml:"is_login"`
+	Uid      int    `json:"u_id,omitempty" yaml:"u_id,omitempty"`
 }
 
 func (s *SSOClient) IsLogin(path string, data interface{}) (bool, error) {
 	body, status, err := s.SClient.do("POST", path, data, true)
+	fmt.Println(status)
 	if err != nil {
-		fmt.Println(status)
+		fmt.Println("1")
 		return false, err
 	}
 	var lo Login
 	err = json.Unmarshal(body, &lo)
 	if err != nil {
+		fmt.Println("2")
 		return false, err
 	}
 	return lo.Is_login, nil
