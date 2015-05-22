b55d35de8aa4316afdc6f2afb1f294fda881ace5
peilong <524510356@qq.com>
2015-05-22 11:35:10 +0800

    direct access

--- docker-browser.go
+++ docker-browser.go
@@ -30,6 +30,17 @@ type RunRes struct {
 	Status  int
 	Message string
 }
+type Image struct{
+	Imagename string
+}
+type LBInstance struct{
+	ServerIp string
+	ServerPort string
+}
+type LBContent struct{
+	Status int
+	Instance LBInstance
+}
 
 func NewDockerClient(endpoint string) (*DockerClient, error) {
 	c, err := newClient(endpoint)
@@ -40,7 +51,22 @@ func NewDockerClient(endpoint string) (*DockerClient, error) {
 		Docker: c,
 	}, nil
 }
-
+func (d *DockerClient) GetIBAddr(lbaddr string,image string)(*LBContent,error){
+	ld,err :=newClient("http://192.168.0.196:3000")
+	if err != nil{
+		return nil,err	
+	}
+	data := Image{
+		Imagename:image,
+	}
+	body,_,err := ld.do("POST","/api/dispatcher/v1.0/container/create",data,true,nil)
+	var li LBContent
+	err = json.Unmarshal(body.&li)
+	if err != nil{
+		return nil,err
+	}
+	return &li,nil
+}
 func (d *DockerClient) DirectDockerRun(data RunData) (*RunRes, error){
 
 	body,_,err := d.Docker.do("POST","/api/coderunner",data,true,nil)
